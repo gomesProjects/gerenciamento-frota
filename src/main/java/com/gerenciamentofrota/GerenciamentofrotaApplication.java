11c549cc9c70eb0fcc17df081c57fbbf9296d8fd
gomesProjects <igorbarros.ti.estadual@gmail.com>
2021-03-24 19:37:46 -0300

    Estrutura do projeto

---
+++ src/main/java/com/gerenciamentofrota/GerenciamentofrotaApplication.java
@@ -0,0 +1,13 @@
+package com.gerenciamentofrota;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentofrotaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentofrotaApplication.class, args);
+	}
+
+}
