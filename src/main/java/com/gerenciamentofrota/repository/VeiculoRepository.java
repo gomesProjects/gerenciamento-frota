d4a96b28aafa539fe8493c14ac1076aa8b36bbe2
gomesProjects <igorbarros.ti.estadual@gmail.com>
2021-03-24 20:59:47 -0300

    Metodos adicionais acrescentados em VeiculoRepository

--- src/main/java/com/gerenciamentofrota/repository/VeiculoRepository.java
+++ src/main/java/com/gerenciamentofrota/repository/VeiculoRepository.java
@@ -2,6 +2,17 @@ package com.gerenciamentofrota.repository;
 
 import com.gerenciamentofrota.model.Veiculo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+
+    @Query(value = "select * from veiculo left join modelo" +
+            "where modelo.nome = ?1", nativeQuery = true)
+    List<Veiculo> findByModelo(String modelo);
+
+    @Query(value = "select * from veiculo left join marca" +
+            "where marca.nome = ?1", nativeQuery = true)
+    List<Veiculo> findByMarca(String marca);
 }
