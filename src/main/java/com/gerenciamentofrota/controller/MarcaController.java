c2a51bbc91aa45c2cf644c95629e0015d6160caa
gomesProjects <igorbarros.ti.estadual@gmail.com>
2021-03-24 21:37:52 -0300

    Corrigindo erros nos Requestmappings

--- src/main/java/com/gerenciamentofrota/controller/MarcaController.java
+++ src/main/java/com/gerenciamentofrota/controller/MarcaController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("veiculo")
+@RequestMapping("marca")
 public class MarcaController extends DefaultController<Marca, MarcaService> {
 }
--- src/main/java/com/gerenciamentofrota/controller/ModeloController.java
+++ src/main/java/com/gerenciamentofrota/controller/ModeloController.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("veiculo")
+@RequestMapping("modelo")
 public class ModeloController extends DefaultController<Modelo, ModeloService> {
 }
